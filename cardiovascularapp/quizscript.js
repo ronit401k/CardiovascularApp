23af82b76a7c14a0c90e4fa3fad6a737779624de
ronit401k <ronit401k@gmail.com>
2025-10-06 12:39:29 +0000

    Updates to the App

--- cardiovascularapp/quizscript.js
+++ cardiovascularapp/quizscript.js
@@ -1,6 +1,16 @@
 document.getElementById("quiz-submit").addEventListener("click",getAnswers)
 function getAnswers(event){
-  document
+  var results=[]
+  var formElem=document.forms.quizform
+  
+  var formData=new FormData(formElem)
+  for(let i=1;i<=6; i++){
+    results.push(formData.getAll("q"+i))
+  }
+  console.log(results)
+  sessionStorage.setItem("quizResults",JSON.stringify(results))
+  window.location.href="answers.html"
 }
+
 
 // Move sections to separate pages first
